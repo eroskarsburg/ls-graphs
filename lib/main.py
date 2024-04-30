3cc96555f9f5c8f57b48843b090c3e13cff7f18a
Eros Karsburg <eroskarsburg@gmail.com>
2024-04-30 11:59:01 -0300

    feat: added mini_map.txt

--- lib/main.py
+++ lib/main.py
@@ -1,14 +1,14 @@
 """Utilize este arquivo para depurar seus algoritmos."""
 
 from graph import read_graph
-from search import dfs, bfs, a_star, dijkstra, branch_and_bound
+from search import dfs as d, bfs as b, a_star as aS, dijkstra as dij, branch_and_bound as bab
 
 
 if __name__ == "__main__":
     grafo = read_graph("mapas\mini_map.txt")
 
-    print(bfs(grafo, 0, 7))
-    #print(dfs(grafo, 0, 7))
-    #print(branch_and_bound(grafo, 0, 7))
-    #print(a_star(grafo, 0, 7))
-    #print(dijkstra(grafo, 0, 7))
+    print(b.bfs(grafo, 0, 7))
+    #print(d.dfs(grafo, 0, 7))
+    #print(bab.branch_and_bound(grafo, 0, 7))
+    #print(aS.a_star(grafo, 0, 7))
+    #print(dij.dijkstra(grafo, 0, 7))
