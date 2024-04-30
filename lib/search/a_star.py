9c381c870e6520899f8941817baa9b14aa93290a
Eros Karsburg <eroskarsburg@gmail.com>
2024-04-30 11:41:57 -0300

    feat: a_star, branch_and_bound implementations

--- lib/search/a_star.py
+++ lib/search/a_star.py
@@ -1,39 +1,12 @@
-from typing import List, Tuple
-
 """Implementação do algoritmo A*."""
-# Definição da estrutura do grafo
-class Graph:
-    def __init__(self, num_nodes: int):
-        self.num_nodes = num_nodes
-        self.adjacency_list = [[] for _ in range(num_nodes)]
-        self.coordinates = {}
-
-    def add_node(self, node: int, latitude: float, longitude: float):
-        self.coordinates[node] = (latitude, longitude)
-
-    def add_edge(self, node1: int, node2: int, cost: float):
-        self.adjacency_list[node1].append((node2, cost))
-        self.adjacency_list[node2].append((node1, cost))
+from typing import List, Tuple
+from lib.graph import Graph
 
 # Função para calcular a distância (heurística) entre dois nós usando a distância euclidiana
 def euclidean_distance(node1_coords: Tuple[float, float], node2_coords: Tuple[float, float]) -> float:
     return ((node1_coords[0] - node2_coords[0]) ** 2 + (node1_coords[1] - node2_coords[1]) ** 2) ** 0.5
 
-# Função para ler o grafo a partir de um arquivo
-def read_graph(filename: str) -> Graph:
-    with open(filename, 'r') as file:
-        num_nodes = int(file.readline())
-        graph = Graph(num_nodes)
-        for _ in range(num_nodes):
-            node, latitude, longitude = map(float, file.readline().split())
-            graph.add_node(int(node), latitude, longitude)
-        num_edges = int(file.readline())
-        for _ in range(num_edges):
-            node1, node2, cost = map(int, file.readline().split())
-            graph.add_edge(node1, node2, cost)
-    return graph
 
-# Implementação do algoritmo A*
 def a_star(graph: Graph, start: int, goal: int) -> (int, float, [int]):
     """Busca em graph, um caminho entre start e goal usando A*."""
     num_nodes_explored = 0
---
+++ lib/search/branch_and_bound.py
@@ -0,0 +1,26 @@
+from lib.graph import Graph
+from typing import List, Tuple
+
+def branch_and_bound(graph: Graph, start: int, goal: int) -> Tuple[int, float, List[int]]:
+    num_nodes_explored = 0
+    shortest_path_length = float('inf')
+    shortest_path = []
+
+    def branch_and_bound_helper(current_node: int, path: List[int], path_length: float):
+        nonlocal num_nodes_explored, shortest_path_length, shortest_path
+        num_nodes_explored += 1
+
+        if current_node == goal:
+            if path_length < shortest_path_length:
+                shortest_path_length = path_length
+                shortest_path = path.copy()
+            return
+
+        for neighbor, cost in graph.adjacency_list[current_node]:
+            if neighbor not in path:
+                if path_length + cost < shortest_path_length:
+                    branch_and_bound_helper(neighbor, path + [neighbor], path_length + cost)
+
+    branch_and_bound_helper(start, [start], 0)
+
+    return num_nodes_explored, shortest_path_length, shortest_path
